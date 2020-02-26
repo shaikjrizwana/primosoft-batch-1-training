67b9b7dfdb9f676a4a045bab8e54d4869f394342
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-02-26 21:58:23 +0530

    code is modified

--- is-even/isEven.js
+++ is-even/isEven.js
@@ -1,6 +1,12 @@
 
-function isEven(...args) {
-  return args;
+function isEven(num) {
+  var quot;
+  quot=Math.floor(num/2);
+  if(quot*2==num)
+  {
+    return true;
+  }
+  return false;
 }
 
 export {
