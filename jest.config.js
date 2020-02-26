d0fe6313b38e649b666b8f3cdddcf5a9c83f45ee
Abdul Khuddus <khuddus1@gmail.com>
2020-02-26 17:07:51 +0530

    feat: practice problems for day-3

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,3 @@
 module.exports = {
   verbose: true,
-  reporters: [
-    'default',
-  ],
 };
--- your-name/yourName.test.js
+++ your-name/yourName.test.js
@@ -1,7 +1,7 @@
 import { yourName } from './yourName';
 
 describe('Write your name in the test', () => {
-  test('yourName should return my name', () => {
+  test('yourName should return your name', () => {
     expect(yourName()).toBe('Write your name here!');
   });
 });
