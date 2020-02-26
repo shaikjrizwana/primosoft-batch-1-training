fbfc0dc194b870aa4b0554df58d22f427296707b
MahendraKondadasu <45584656+MahendraKondadasu@users.noreply.github.com>
2020-02-26 22:51:07 +0530

    Update isEven.js

--- is-even/isEven.js
+++ is-even/isEven.js
@@ -1,12 +1,6 @@
 
-function isEven(num) {
-  var quot;
-  quot=Math.floor(num/2);
-  if(quot*2==num)
-  {
-    return true;
-  }
-  return false;
+function isEven(...args) {
+  return args
 }
 
 export {
