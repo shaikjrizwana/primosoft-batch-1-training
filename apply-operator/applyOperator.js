f20a5394916f7a2428ec689cf76d731cdc846dda
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-02-27 12:53:39 +0530

    code is modified

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,6 +1,50 @@
 
 function applyOperator(...args) {
-  return args;
+  if (args.length == 0) {
+    throw new SyntaxError();
+  }
+  var operator = args[0];
+  var result;
+  if (args.length == 1) {
+    return 0;
+  }
+
+  switch (operator) {
+    case '+':
+      result = args[1];
+      for (var i = 2; i < args.length; i++) {
+        result += args[i];
+      }
+      break;
+    case '-':
+      result = -args[1];
+      for (var i = 2; i < args.length; i++) {
+        result -= args[i];
+      }
+      break;
+    case '*':
+      result = args[1];
+      for (var i = 2; i < args.length; i++) {
+        result *= args[i];
+      }
+
+      break;
+    case '/':
+      result = args[1];
+      for (var i = 2; i < args.length; i++) {
+        result /= args[i];
+      }
+      result = result.toFixed(4);
+      result = parseFloat(result);
+      break;
+    case '%':
+      result = args[1];
+      for (var i = 2; i < args.length; i++) {
+        result %= args[i];
+      }
+      break;
+  }
+  return result;
 }
 
 export {
