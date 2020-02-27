aefa6e79cbffd9ab8417d9aec8990755f44b974a
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-02-27 11:09:30 +0530

    code is modified

--- is-triangle/isTriangle.js
+++ is-triangle/isTriangle.js
@@ -1,9 +1,9 @@
 
-function isTriangle(side1,side2,side3) {
-  if(side1+side2<=side3 || side2+side3<=side1 || side1+side3<=side2){
+function isTriangle(side1, side2, side3) {
+  if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2) {
     return false;
   }
-  
+
   return true;
 }
 
