08fed6497169fb6fe815802655d4b1b5ff88a99f
Divya436 <DivyaChinthala519@gmail.com>
2020-02-27 16:58:32 +0530

    isTriangle code changed

--- is-triangle/isTriangle.js
+++ is-triangle/isTriangle.js
@@ -1,6 +1,11 @@
 
-function isTriangle(...args) {
-  return args;
+function isTriangle(a,b,c) {
+  if((a+b)>c & (b+c)>a & (c+a)>b){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 export {
