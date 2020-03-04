3dfbef97ab0af81a18daffb9f3ff20b338e2b9ba
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-04 14:12:42 +0530

    changed  variable declaration from var to const

--- rectangle-diagonal/rectangleDiagonal.js
+++ rectangle-diagonal/rectangleDiagonal.js
@@ -1,9 +1,6 @@
-
 function rectangleDiagonal(length, breadth) {
-  var lenDiag, sumOfSquares;
-  sumOfSquares = Math.pow(length, 2) + Math.pow(breadth, 2);
-  lenDiag = Math.sqrt(sumOfSquares);
-  return lenDiag;
+  const sumOfSquares = Math.pow(length, 2) + Math.pow(breadth, 2);
+  return Math.sqrt(sumOfSquares);
 }
 
 export {
