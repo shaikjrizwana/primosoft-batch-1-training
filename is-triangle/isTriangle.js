eec82d0502f95803da305ca5af2854109e930aec
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-04 14:18:36 +0530

    formatted the code

--- is-triangle/isTriangle.js
+++ is-triangle/isTriangle.js
@@ -1,12 +1,13 @@
-
 function isTriangle(side1, side2, side3) {
-  if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2) {
+  if (
+    side1 + side2 <= side3 ||
+    side2 + side3 <= side1 ||
+    side1 + side3 <= side2
+  ) {
     return false;
   }
 
   return true;
 }
 
-export {
-  isTriangle,
-};
+export { isTriangle };
