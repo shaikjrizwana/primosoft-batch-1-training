45905be89308bf69ba650edf1e2ed9d954cf8578
Abdul Khuddus <khuddus1@gmail.com>
2020-03-06 19:01:00 +0530

    feat: chunked array version 1

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,6 +1,19 @@
 
-function chunkArrayInGroups(...args) {
-  return args;
+function chunkArrayInGroups(arr, size) {
+  const finalArray = [];
+  let chunkedArray = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    chunkedArray.push(arr[i]);
+    
+    if (chunkedArray.length === size) {
+      finalArray.push(chunkedArray);
+      chunkedArray = [];
+    }
+  }
+  if (chunkedArray.length > 0) {
+    finalArray.push(chunkedArray);
+  }
+  return finalArray;
 }
 
 export {
