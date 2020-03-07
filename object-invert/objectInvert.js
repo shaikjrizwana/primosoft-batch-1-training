7735c53057e7e5a513fff4c4468ba853d4c2b3d6
Divya436 <DivyaChinthala519@gmail.com>
2020-03-07 22:01:16 +0530

    made changes in objectInvert

--- object-invert/objectInvert.js
+++ object-invert/objectInvert.js
@@ -1,11 +1,14 @@
-
 function objectInvert(object) {
-  const finalObject = {};
-  for (const key in object) {
-    finalObject[object[key]] = key;
-  }
-
-  return finalObject;
+  const array = (Object.entries(object).map((pair) => {
+    const obj = {};
+    obj[pair[1]] = pair[0];
+    return obj;
+    })).reduce((result, pair) => {
+    const key = Object.keys(pair);
+    result[key] = pair[key];
+    return result;
+  }, {});
+  return array;
 }
 
 export {
