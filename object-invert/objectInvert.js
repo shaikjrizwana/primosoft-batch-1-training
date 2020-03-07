6e5cd5e865a9194d6e85d3893dd3aaa810f53b14
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-07 21:40:15 +0530

    objectInvert using reduce()

--- object-invert/objectInvert.js
+++ object-invert/objectInvert.js
@@ -1,8 +1,8 @@
-
-function objectInvert(...args) {
-  return args;
+function objectInvert(obj) {
+  return Object.keys(obj).reduce((acc, key) => {
+    acc[obj[key]] = key;
+    return acc;
+  }, {});
 }
 
-export {
-  objectInvert,
-};
+export { objectInvert };
