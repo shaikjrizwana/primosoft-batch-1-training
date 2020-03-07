4a5849da6c863dd6f4fc242f7ff49772faf55802
Divya436 <DivyaChinthala519@gmail.com>
2020-03-07 11:07:59 +0530

    made changes in objectValues

--- object-values/objectValues.js
+++ object-values/objectValues.js
@@ -1,8 +1,12 @@
-
-function objectValues(...args) {
-  return args;
+function objectValues(obj) {
+  const arr = [];
+  for (const i in obj) {
+    arr.push(obj[i]);
+  }
+  if (Array.isArray(arr)) {
+    return arr;
+  }
 }
-
 export {
   objectValues,
 };
