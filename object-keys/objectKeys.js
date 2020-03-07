9b709a0fe11161077a5e3f517ad6fdb907118b91
Divya436 <DivyaChinthala519@gmail.com>
2020-03-07 14:50:02 +0530

    made changes in objectKeys using map

--- object-keys/objectKeys.js
+++ object-keys/objectKeys.js
@@ -1,10 +1,7 @@
 
 function objectKeys(object) {
-  const array = [];
-  for (var key in object) {
-    array.push(key);
-  }
-  if(Array.isArray(array)){
+  const array = Object.entries(object).map((pair) => pair[0]);
+  if (Array.isArray(array)) {
     return array;
   }
 }
