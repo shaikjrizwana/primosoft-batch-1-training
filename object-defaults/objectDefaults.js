17c3629d6e4dececb68d232160ff3d5aecd27f64
Divya436 <DivyaChinthala519@gmail.com>
2020-03-08 09:58:41 +0530

    made changes in objectDefaults using reduce

--- object-defaults/objectDefaults.js
+++ object-defaults/objectDefaults.js
@@ -1,11 +1,18 @@
 
-function objectDefaults(object, defaultobject) {
-  for (const key in defaultobject) {
-    if (!(key in object)) {
-      object[key] = defaultobject[key];
+function objectDefaults(object, defaultobjects) {
+  const array = [];
+  array.push(object);
+  array.push(defaultobjects);
+  const finalObject = array.reduce((result, obj) => {
+    const keys = Object.keys(obj);
+    for (let i = 0; i < keys.length; i += 1) {
+      if (!(keys[i] in result)) {
+        result[keys[i]] = obj[keys[i]];
+      }
     }
-  }
-  return object;
+    return result;
+  }, {});
+  return finalObject;
 }
 
 export {
