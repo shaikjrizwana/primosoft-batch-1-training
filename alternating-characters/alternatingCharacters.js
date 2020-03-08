ca7e0eb1a8a884fba54962c49b2fe237f80e391f
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-08 23:29:54 +0530

    made changes is alternatingCharacters using loops

--- alternating-characters/alternatingCharacters.js
+++ alternating-characters/alternatingCharacters.js
@@ -1,8 +1,16 @@
-
-function alternatingCharacters(...args) {
-  return args;
+function alternatingCharacters(arr) {
+  const minNoOfDeletions = [];
+  for (let i = 0; i < arr.length; i++) {
+    const string = arr[i];
+    let count = 0;
+    for (let j = 0; j < string.length - 1; j++) {
+      if (string[j] == string[j + 1]) {
+        count++;
+      }
+    }
+    minNoOfDeletions.push(count);
+  }
+  return minNoOfDeletions;
 }
 
-export {
-  alternatingCharacters,
-};
+export { alternatingCharacters };
