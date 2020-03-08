4634badb298a4cc42e2651a4fa1c16c9cde5797a
Divya436 <DivyaChinthala519@gmail.com>
2020-03-08 10:17:10 +0530

    made changes in mapObject

--- map-object/mapObject.js
+++ map-object/mapObject.js
@@ -1,8 +1,13 @@
-
-function mapObject(...args) {
-  return args;
+function mapObject(object) {
+  const finalObject = Object.entries(object).map((pair) => {
+    pair[1] *= pair[1];
+    return pair;
+  }).reduce((result, pair) => {
+    result[pair[0]] = pair[1];
+    return result;
+  }, {});
+  return finalObject;
 }
-
 export {
   mapObject,
 };
