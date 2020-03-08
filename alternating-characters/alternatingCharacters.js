aaac43f16d0c04d57bb1e46a6ced19c1be52cff8
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-08 23:48:18 +0530

    alternatingCharacters using reduce

--- alternating-characters/alternatingCharacters.js
+++ alternating-characters/alternatingCharacters.js
@@ -1,16 +1,14 @@
 function alternatingCharacters(arr) {
-  const minNoOfDeletions = [];
-  for (let i = 0; i < arr.length; i++) {
-    const string = arr[i];
+  return arr.reduce((acc, string, index) => {
     let count = 0;
     for (let j = 0; j < string.length - 1; j++) {
       if (string[j] == string[j + 1]) {
         count++;
       }
     }
-    minNoOfDeletions.push(count);
-  }
-  return minNoOfDeletions;
+    acc.push(count);
+    return acc;
+  }, []);
 }
 
 export { alternatingCharacters };
