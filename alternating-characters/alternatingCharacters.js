ed98bc2baf5c78c32b12f9fb99214ad35921012d
Divya436 <DivyaChinthala519@gmail.com>
2020-03-09 12:07:47 +0530

    made changes in alternatingCharacters

--- alternating-characters/alternatingCharacters.js
+++ alternating-characters/alternatingCharacters.js
@@ -1,6 +1,24 @@
 
-function alternatingCharacters(...args) {
-  return args;
+function alternatingCharacters(strings) {
+  const final = strings.reduce((result, string) => {
+    const arr = string.split('');
+    const arr1 = string.split('');
+    let deletions = 0;
+    let i = 0;
+    for (let index = 1; index < arr.length; index++) {
+      if (arr[i] === arr[index]) {
+        arr1.splice(index, 1);
+        arr1[index] = '';
+        deletions += 1;
+      } else {
+        i = index;
+      }
+    }
+    result.push(deletions);
+    return result;
+    
+  }, []);
+  return final;
 }
 
 export {
