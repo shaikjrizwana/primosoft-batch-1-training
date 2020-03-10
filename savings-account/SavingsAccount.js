d04ee311ff36a8ddf9eabf16c4f1ba43cc1747e4
Divya436 <DivyaChinthala519@gmail.com>
2020-03-10 21:05:33 +0530

    made changes in SavingsAccount

--- savings-account/SavingsAccount.js
+++ savings-account/SavingsAccount.js
@@ -1,8 +1,25 @@
+/* eslint-disable no-underscore-dangle */
 
-function savingsAccount(...args) {
-  return args;
+function SavingsAccount(_accountNumber, _email, _firstName, _lastName) {
+  this._accountNumber = _accountNumber;
+  this._email = _email;
+  this._firstName = _firstName;
+  this._lastName = _lastName;
+  this.products = [];
+  if (this._accountNumber.length != 6) {
+    throw new Error('Account Number must be a 6-digit number');
+  }
+  if (!(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]+$/.test(this._email))) {
+    throw new Error('Invalid e-mail');
+  }
+  if (!(this._firstName.length > 3 && this._firstName.length < 20)) {
+    throw new Error('First name must be between 3 and 20 characters long');
+  }
+  if (!(/^[a-zA-Z]+$/.test(this._lastName))) {
+    throw new Error('Last name must contain english alphabets only');
+  }
 }
 
 export {
-  savingsAccount,
+  SavingsAccount,
 };
