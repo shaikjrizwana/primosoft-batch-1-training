fbf11bcd9c339da9dcfcd6105e7e545f6621b13a
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-11 11:59:29 +0530

    made changes in abbreviateString

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,6 +1,15 @@
+function abbreviateString(string) {
+  if (typeof string === 'string') {
+    const stringArray = string.split(' ');
+    const firstWrod = stringArray[0];
+    const lastWord = stringArray[stringArray.length - 1];
+    const lastWordFirstLetter = lastWord[0].toUpperCase();
+    let finalString = '';
+    finalString = finalString.concat(firstWrod, ' ', lastWordFirstLetter, '.');
 
-function abbreviateString(...args) {
-  return args;
+    return finalString;
+  }
+  throw Error('Invalid parameters');
 }
 
 export {
