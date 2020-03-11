839b365b96dbf883f455cf7f52ca0aa8a07bb19c
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-11 14:31:09 +0530

    modified parseMs one test case passed.

--- parse-ms/parseMs.js
+++ parse-ms/parseMs.js
@@ -1,8 +1,29 @@
+function parseMs(seconds) {
+  let totalMilliSeconds = seconds;
 
-function parseMs(...args) {
-  return args;
+  const noOfDays = Math.floor(totalMilliSeconds / (24 * 3600 * 1000));
+  totalMilliSeconds %= 24 * 3600 * 1000;
+
+  const noOfHours = Math.floor(totalMilliSeconds / (3600 * 1000));
+  totalMilliSeconds %= 3600 * 1000;
+
+  const noOfMinutes = Math.floor(totalMilliSeconds / (60 * 1000));
+  totalMilliSeconds %= 60 * 1000;
+
+  const noOfSeconds = Math.floor(totalMilliSeconds / 1000);
+  totalMilliSeconds %= 1000;
+
+  const noOfMilliSeconds = totalMilliSeconds;
+
+  const result = {
+    days: noOfDays,
+    hours: noOfHours,
+    minutes: noOfMinutes,
+    seconds: noOfSeconds,
+    milliseconds: noOfMilliSeconds
+  };
+
+  return result;
 }
 
-export {
-  parseMs,
-};
+export { parseMs };
