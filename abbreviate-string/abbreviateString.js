74986252b860c15a699c97082c2f12758bbc751f
Divya436 <DivyaChinthala519@gmail.com>
2020-03-11 11:15:35 +0530

    made changes in abbreviateString

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,6 +1,13 @@
 
-function abbreviateString(...args) {
-  return args;
+function abbreviateString(string) {
+  if(typeof string == "string"){
+    const f = string.indexOf(' ');
+    const i = string.lastIndexOf(' ');
+    return string.slice(0,f) + ' ' + string[i+1].toUpperCase() + '.';
+  }
+  else{
+    throw new Error();
+  }
 }
 
 export {
