669bc2cb6b10eebf69c804d68e885affa4fadb42
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-12 10:52:07 +0530

    made changes in curry

--- curry/curry.js
+++ curry/curry.js
@@ -1,6 +1,7 @@
-
-function curry(...args) {
-  return args;
+function curry(fn, ...args) {
+  return fn.length <= args.length
+    ? fn(...args)
+    : (...more) => curry(fn, ...args, ...more);
 }
 
 export {
