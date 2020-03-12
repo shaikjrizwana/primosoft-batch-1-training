73f28d949c4df10ad58a41dfde2739e7ed2b9839
Divya436 <DivyaChinthala519@gmail.com>
2020-03-12 14:52:10 +0530

    made changes in mathFns

--- math-fns/mathFns.js
+++ math-fns/mathFns.js
@@ -1,8 +1,13 @@
 
-function mathFns(...args) {
-  return args;
+function sqrt(number) {
+  return Math.sqrt(number);
 }
-
+function power(value, exponent) {
+  return Math.pow(value, exponent);
+}
+function round(number) {
+  return Math.round(number);
+}
 export {
-  mathFns,
+  sqrt, power, round,
 };
