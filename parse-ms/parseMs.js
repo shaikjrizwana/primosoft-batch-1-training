6a56226a31fba5056a52bea0820917be55bf4cf2
Divya436 <DivyaChinthala519@gmail.com>
2020-03-12 16:25:10 +0530

    made changes in parseMs

--- parse-ms/parseMs.js
+++ parse-ms/parseMs.js
@@ -1,6 +1,24 @@
 
-function parseMs(...args) {
-  return args;
+function parseMs(ms) {
+  let s = Math.floor(ms / 1000);
+  let m = Math.floor(s / 60);
+  s %= 60;
+  let h = Math.floor(m / 60);
+  m %= 60;
+  const d = Math.floor(h / 24);
+  h %= 24;
+  const millis = Math.floor((ms % 1000) * 1000) / 1000;
+  const object = {
+    days: d, hours: h, minutes: m, seconds: s, milliseconds: millis,
+  };
+  if (ms < 0) {
+    return (Object.keys(object).reduce((res, key) => {
+      res[key] = -(object[key]);
+      console.log(res);
+      return res;
+    }, {}));
+  }
+  return object;
 }
 
 export {
