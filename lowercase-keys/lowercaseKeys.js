4535488f607dbaadcfe3394d0e813ddd7b62acc1
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-12 10:59:14 +0530

    made changes in lowercaseKeys

--- lowercase-keys/lowercaseKeys.js
+++ lowercase-keys/lowercaseKeys.js
@@ -1,8 +1,8 @@
-
-function lowercaseKeys(...args) {
-  return args;
+function lowercaseKeys(obj) {
+  return Object.keys(obj).reduce((acc, key) => {
+    acc[key.toLocaleLowerCase()] = obj[key];
+    return acc;
+  }, {});
 }
 
-export {
-  lowercaseKeys,
-};
+export { lowercaseKeys };
