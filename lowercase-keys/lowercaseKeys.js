ca279ddbeca23f4362bf54e5fe7f14ad7b872085
Divya436 <DivyaChinthala519@gmail.com>
2020-03-12 09:49:08 +0530

    made changes in lowercaseKeys

--- lowercase-keys/lowercaseKeys.js
+++ lowercase-keys/lowercaseKeys.js
@@ -1,6 +1,9 @@
 
-function lowercaseKeys(...args) {
-  return args;
+function lowercaseKeys(object) {
+  return Object.keys(object).reduce((result, key) => {
+    result[key.toLowerCase()] = object[key];
+    return result;
+  }, {});
 }
 
 export {
