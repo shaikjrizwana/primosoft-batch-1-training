74b97f8189eed103e602ee489a0c346b6a98768f
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-13 22:51:08 +0530

    made changes in mapFilterReduce

--- map-filter-and-reduce/mapFilterAndReduce.js
+++ map-filter-and-reduce/mapFilterAndReduce.js
@@ -1,6 +1,9 @@
 
-function mapFilterAndReduce(...args) {
-  return args;
+function mapFilterAndReduce(arr) {
+  return arr.map((obj) => (obj.firstName)).filter((name) => (name.length < 5)).reduce((result, name) => {
+    result[name] = name.length;
+    return result;
+  }, {});
 }
 
 export {
