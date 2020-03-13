abbb28a8920a3c7a3d7c7fe9a5d2a9bcbfef96d4
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-13 23:45:59 +0530

    made changes in templateLiterals

--- template-literals/templateLiterals.js
+++ template-literals/templateLiterals.js
@@ -1,6 +1,6 @@
 
-function templateLiterals(...args) {
-  return args;
+function templateLiterals() {
+  return `There are 4 people on the football team. Their names are Rooney, Ronaldo, Messi, Pogba.`;
 }
 
 export {
