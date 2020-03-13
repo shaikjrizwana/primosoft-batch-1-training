4d4cdff80cdded9b1b87f06d51845bc170ed23b6
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-13 23:44:28 +0530

    made changes in placeMiddle

--- place-in-middle/placeInMiddle.js
+++ place-in-middle/placeInMiddle.js
@@ -1,6 +1,8 @@
 
-function placeInMiddle(...args) {
-  return args;
+function placeInMiddle(arr, MiddleArray) {
+  const startArray = arr;
+  const endArray = startArray.splice(Math.floor(startArray.length / 2));
+  return startArray.concat(MiddleArray, endArray);
 }
 
 export {
