29d8cba54d93d295f8846391868bf6bc571cb8e1
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-13 16:05:06 +0530

    made changes in tripleFilter

--- triple-and-filter/tripleAndFilter.js
+++ triple-and-filter/tripleAndFilter.js
@@ -1,8 +1,8 @@
 
-function tripleAndFilter(...args) {
-  return args;
-}
+function tripleAndFilter(numbers) {
+  return numbers.map((num) => num * 3).filter((num)=> num % 5 === 0);
 
+}
 export {
   tripleAndFilter,
 };
