2adfad23d69b11b0d2cc09032080312afd5a8a04
Divya436 <DivyaChinthala519@gmail.com>
2020-03-13 22:19:53 +0530

    made changes in sumEvenArgs

--- sum-even-args/sumEvenArgs.js
+++ sum-even-args/sumEvenArgs.js
@@ -1,6 +1,9 @@
 
 function sumEvenArgs(...args) {
-  return args;
+  return args.filter((val) => val % 2 === 0).reduce((result, element) => {
+    result += element;
+    return result;
+  }, 0);
 }
 
 export {
