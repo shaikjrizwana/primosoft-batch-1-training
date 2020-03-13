451232b6109b2cc791b8fd6ce5f2a1930d707619
Divya436 <DivyaChinthala519@gmail.com>
2020-03-13 17:56:37 +0530

    made changes in memoize

--- memoize/memoize.js
+++ memoize/memoize.js
@@ -1,6 +1,16 @@
 
-function memoize(...args) {
-  return args;
+function memoize(func) {
+  const cache = {};
+  return (...args) => {
+    const input = args[0];
+    if (input in cache) {
+      return cache[input];
+    }
+    const result = func(input);
+    cache[input] = result;
+    // console.log(result);
+    return result;
+  };
 }
 
 export {
