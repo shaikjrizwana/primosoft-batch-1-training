1e5220c6064440fbc24201334db6ec686a759485
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-14 00:06:11 +0530

    made changes in createStudent

--- create-student/createStudent.js
+++ create-student/createStudent.js
@@ -1,8 +1,15 @@
-
-function createStudent(...args) {
-  return args;
+function createStudent(obj) {
+  const { likesJavaScript, likesES2015 } = obj || {};
+  if (likesJavaScript === false && likesES2015 === false) {
+    return 'The student does not like much...';
+  }
+  if (likesES2015 === false) {
+    return 'The student likes JavaScript!';
+  }
+  if (likesJavaScript === false) {
+    return 'The student likes ES2015!';
+  }
+  return 'The student likes JavaScript and ES2015';
 }
 
-export {
-  createStudent,
-};
+export { createStudent };
