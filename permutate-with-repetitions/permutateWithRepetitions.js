49a96d73514f7908513d588b7263eeaf0aa7e866
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-13 15:39:41 +0530

    made changes in permutateWithRepetitions

--- permutate-with-repetitions/permutateWithRepetitions.js
+++ permutate-with-repetitions/permutateWithRepetitions.js
@@ -1,20 +1,17 @@
 function permutateWithRepetitions(charArray) {
-   return generatePermutations(charArray, charArray.length);
+  return generatePermutations(charArray, charArray.length);
 }
 
 function generatePermutations(charArray, permutationLength) {
   if (permutationLength === 1) {
-    return charArray.map((char) => [char]);
+    return charArray.map(char => [char]);
   }
   const permutations = [];
 
-  const smallPermutations = generatePermutations(
-    charArray,
-    permutationLength - 1,
-  );
+  const smallPermutations = generatePermutations(charArray, permutationLength - 1);
 
-  charArray.forEach((char) => {
-    smallPermutations.forEach((smallPermutation) => {
+  charArray.forEach(char => {
+    smallPermutations.forEach(smallPermutation => {
       permutations.push([char].concat(smallPermutation));
     });
   });
