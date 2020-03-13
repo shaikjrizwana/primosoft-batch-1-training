e5729288db43e3c4e90996d53071ea4cc57ab8ed
Divya436 <DivyaChinthala519@gmail.com>
2020-03-13 21:53:02 +0530

    made changes in mapFilterAndReduce

--- map-filter-and-reduce/mapFilterAndReduce.js
+++ map-filter-and-reduce/mapFilterAndReduce.js
@@ -1,6 +1,11 @@
 
-function mapFilterAndReduce(...args) {
-  return args;
+function mapFilterAndReduce(array) {
+  return array.map((obj) => obj.firstName).filter((val) => val.length < 5).reduce(
+    (result, value) => {
+      result[value] = value.length;
+      return result;
+    }, {},
+  );
 }
 
 export {
