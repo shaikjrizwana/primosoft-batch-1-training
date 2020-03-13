c3411927a068b117f9a76695340be8d520dae5b6
Divya436 <DivyaChinthala519@gmail.com>
2020-03-13 10:52:32 +0530

    made changes in permutateWithRepetitions

--- permutate-with-repetitions/permutateWithRepetitions.js
+++ permutate-with-repetitions/permutateWithRepetitions.js
@@ -1,8 +1,19 @@
-
-function permutateWithRepetitions(...args) {
-  return args;
+function permutateWithRepetitions(charArray) {
+  const permutation = '';
+  const finalArray = [];
+  printPermutations(charArray, permutation, finalArray);
+  return finalArray;
 }
-
+function printPermutations(charArray, permutation, finalArray) {
+  if (charArray.length === permutation.length) {
+    const array = permutation.split('');
+    finalArray.push(array);
+    return finalArray;
+  }
+  for (let i = 0; i < charArray.length; i += 1) {
+    printPermutations(charArray, permutation + charArray[i], finalArray);
+  }
+}
 export {
   permutateWithRepetitions,
 };
