c093432d5de0cad8888849caa604d340a4c5e7c3
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-13 11:44:53 +0530

    made changes in roundTo

--- round-to/roundTo.js
+++ round-to/roundTo.js
@@ -1,8 +1,20 @@
-
-function roundTo(...args) {
-  return args;
+function roundTo(number, noOfDecimals) {
+  const extraPrecision = Math.pow(10, noOfDecimals + 1);
+  const extraDigit = Math.round(number * extraPrecision) % 10;
+  const applyUpFunction = extraDigit >= 5;
+  if (applyUpFunction) {
+    return roundTo.up(number, noOfDecimals);
+  }
+  return roundTo.down(number, noOfDecimals);
 }
 
-export {
-  roundTo,
+roundTo.up = function up(number, noOfDecimals) {
+  const precision = Math.pow(10, noOfDecimals);
+  return Math.ceil(number * precision) / precision;
 };
+
+roundTo.down = function up(number, noOfDecimals) {
+  const precision = Math.pow(10, noOfDecimals);
+  return Math.floor(number * precision) / precision;
+};
+export { roundTo };
