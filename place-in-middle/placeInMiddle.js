4196504b573d2bbe1f3e4c5f21f5c2aef12915fc
Divya436 <DivyaChinthala519@gmail.com>
2020-03-13 22:13:05 +0530

    made changes in placeInMiddle

--- place-in-middle/placeInMiddle.js
+++ place-in-middle/placeInMiddle.js
@@ -1,6 +1,8 @@
 
-function placeInMiddle(...args) {
-  return args;
+function placeInMiddle(array1, array2) {
+  const index = array1.length / 2;
+  array1.splice(index, 0, ...array2);
+  return array1;
 }
 
 export {
