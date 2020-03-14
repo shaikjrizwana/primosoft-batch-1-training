5468e8365f660cdc1b1cf3068a2f9af7577ca62c
Divya436 <DivyaChinthala519@gmail.com>
2020-03-14 09:33:27 +0530

    made changes in createStudent

--- create-student/createStudent.js
+++ create-student/createStudent.js
@@ -1,6 +1,16 @@
 
-function createStudent(...args) {
-  return args;
+function createStudent(obj) {
+  if (obj === undefined) {
+    return 'The student likes JavaScript and ES2015';
+  }
+  const { likesES2015, likesJavaScript } = obj;
+  if (likesES2015 === undefined) {
+    return 'The student likes ES2015!';
+  }
+  if (likesJavaScript === undefined) {
+    return 'The student likes JavaScript!';
+  }
+  if (!(likesES2015 && likesJavaScript)) { return 'The student does not like much...'; } 
 }
 
 export {
