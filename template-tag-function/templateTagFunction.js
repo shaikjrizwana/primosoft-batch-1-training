6159ef057bd9dd5e1a0afe894219c03a1e0c4736
Divya436 <DivyaChinthala519@gmail.com>
2020-03-14 11:05:22 +0530

    made changes in templateTagFunction

--- template-tag-function/templateTagFunction.js
+++ template-tag-function/templateTagFunction.js
@@ -1,8 +1,10 @@
 
-function templateTagFunction(...args) {
-  return args;
+function html(string) {
+  return string[0];
 }
-
+function callTemplateTagFunction() {
+  return html`The expression 5 &amp;gt; 4 is &amp;quot;true&amp;quot; &amp; 3 &amp;lt; 1 is false`;
+}
 export {
-  templateTagFunction,
+  callTemplateTagFunction,
 };
