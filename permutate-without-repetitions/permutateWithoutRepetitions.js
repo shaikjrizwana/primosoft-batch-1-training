207c2a3772b20db8b453d10de22d807b8a37131c
Divya436 <DivyaChinthala519@gmail.com>
2020-03-16 17:34:50 +0530

    made changes in permutateWithoutRepetitions

--- permutate-without-repetitions/permutateWithoutRepetitions.js
+++ permutate-without-repetitions/permutateWithoutRepetitions.js
@@ -1,8 +1,22 @@
-
-function permutateWithoutRepetitions(...args) {
-  return args;
+function printPermutations(charArray, permutation, index, finalArray) {
+  if (charArray.length === permutation.length) {
+    const array = permutation.split('');
+    finalArray.push(array);
+    return finalArray;
+  }
+  for (let i = 0; i < charArray.length; i += 1) {
+    if (!(index.includes(i))) {
+      printPermutations(charArray, permutation + charArray[i], index + i, finalArray);
+    }
+  }
 }
-
+function permutateWithoutRepetitions(charArray) {
+  const permutation = '';
+  const index = '';
+  const finalArray = [];
+  printPermutations(charArray, permutation, index, finalArray);
+  return finalArray;
+}
 export {
   permutateWithoutRepetitions,
 };
