57a43831424eed1a246e38aeee8aff1c69b712a2
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-17 16:40:33 +0530

    feat: made changes in Person

--- person/Person.js
+++ person/Person.js
@@ -1,8 +1,22 @@
+/* eslint-disable linebreak-style */
 
-function person(...args) {
-  return args;
+class Person {
+  constructor(firstName, lastName, dateOfBirth) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOfBirth = dateOfBirth;
+  }
 }
 
+
+Person.prototype.addDobDigits = function addDobDigits() {
+  const { dateOfBirth } = this;
+  const stringDigitsInDob = dateOfBirth.split('').filter((char) => char !== '/');
+  const DigitsInDob = stringDigitsInDob.map((str) => parseInt(str, 10));
+  const sumOfDigitsInDob = DigitsInDob.reduce((sum, num) => sum + num, 0);
+  return sumOfDigitsInDob;
+};
+
 export {
-  person,
+  Person,
 };
