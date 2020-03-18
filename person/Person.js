dfe5a74dcc8515433698d0971d0e08d20dd43862
Divya436 <DivyaChinthala519@gmail.com>
2020-03-18 13:25:39 +0530

    feat: made changes in Person

--- person/Person.js
+++ person/Person.js
@@ -1,8 +1,17 @@
 
-function person(...args) {
-  return args;
+class Person {
+  constructor(firstName, lastName, dateOfBirth) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOfBirth = dateOfBirth;
+  }
 }
 
+Person.prototype.addDobDigits = function addDobDigits() {
+  const digits = this.dateOfBirth.split('').filter((ele) => ele !== '/');
+  const sum = digits.reduce((result, value) => result + parseInt(value, 10), 0);
+  return sum;
+};
 export {
-  person,
+  Person,
 };
