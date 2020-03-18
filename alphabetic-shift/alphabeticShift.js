ffb0283a74df036af9cad5d8b6dcdce5ec5afc0b
Divya436 <DivyaChinthala519@gmail.com>
2020-03-18 15:09:36 +0530

    feat: made changes in alphabeticShift

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,6 +1,12 @@
 
-function alphabeticShift(...args) {
-  return args;
+function alphabeticShift(string) {
+  const charArray = string.split('');
+  for (let index = 0; index < string.length; index += 1) {
+    const asciiVal = string.charCodeAt(index);
+    charArray[index] = String.fromCharCode(asciiVal + 1);
+  }
+
+  return charArray.join('');
 }
 
 export {
