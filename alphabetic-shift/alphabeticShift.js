970090c7d902bd37298e878e98cbcc0aef47328b
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-18 13:07:58 +0530

    feat: made changes in alphabeticShift

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,6 +1,11 @@
 
-function alphabeticShift(...args) {
-  return args;
+function alphabeticShift(str) {
+  const charArray = str.split('');
+  charArray.map((char, index) => {
+    const asciiVal = char.charCodeAt(0);
+    charArray[index] = String.fromCharCode(asciiVal + 1);
+  });
+  return charArray.join('');
 }
 
 export {
