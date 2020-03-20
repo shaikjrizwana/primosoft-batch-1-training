8e761e8163786513ba7691fe5202c70e28841d97
Divya436 <DivyaChinthala519@gmail.com>
2020-03-20 21:03:50 +0530

    feat: made changes in animalSort

--- animal-sort/animalSort.js
+++ animal-sort/animalSort.js
@@ -1,6 +1,13 @@
 
-function animalSort(...args) {
-  return args;
+function animalSort(arr) {
+  return arr.sort((ele1, ele2) => {
+    if (ele1.numberOfLegs === ele2.numberOfLegs) {
+      const name1 = ele1.name.toLowerCase();
+      const name2 = ele2.name.toLowerCase();
+      return name1 < name2 ? -1 : name1 > name2 ? 1 : 0;
+    }
+    return ele1.numberOfLegs - ele2.numberOfLegs;
+  });
 }
 
 export {
