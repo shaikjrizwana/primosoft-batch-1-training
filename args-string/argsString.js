0c4e18b87dfb99004684d89a31b7a6340dbd2572
Divya436 <DivyaChinthala519@gmail.com>
2020-03-21 10:46:07 +0530

    feat: made changes in argsString

--- args-string/argsString.js
+++ args-string/argsString.js
@@ -1,6 +1,9 @@
 
-function argsString(...args) {
-  return args;
+function argsString(string, arr) {
+  return arr.reduce((result, val) => {
+    result = result.replace('{}', val);
+    return result;
+  }, string);
 }
 
 export {
