e5f411cabcb529da915abcd176fb6b8164a4f345
Divya436 <DivyaChinthala519@gmail.com>
2020-03-21 22:37:37 +0530

    feat: made changes in Queue

--- queue/Queue.js
+++ queue/Queue.js
@@ -1,8 +1,24 @@
 
-function queue(...args) {
-  return args;
+function Queue() {
+  this.elements = [];
 }
-
+Queue.prototype.peek = function () {
+  if (!this.isEmpty()) {
+    return this.elements[0];
+  }
+};
+Queue.prototype.enqueue = function (ele) {
+  return this.elements.push(ele);
+};
+Queue.prototype.dequeue = function () {
+  return this.elements.shift();
+};
+Queue.prototype.isEmpty = function () {
+  return this.elements.length === 0;
+};
+Queue.prototype.toString = function () {
+  return this.elements.toString();
+};
 export {
-  queue,
+  Queue,
 };
