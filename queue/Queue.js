ed97a96e1d9c92bd1642eb3d8c29e9a4398aed6e
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-21 18:38:41 +0530

    feat: made changes in Queue

--- queue/Queue.js
+++ queue/Queue.js
@@ -1,8 +1,27 @@
-
-function queue(...args) {
-  return args;
+function Queue() {
+  this.queue = [];
 }
-
-export {
-  queue,
+Queue.prototype.enqueue = function enqueue(val) {
+  this.queue.push(val);
 };
+
+Queue.prototype.toString = function toString(stringfier) {
+  if (typeof stringfier !== 'function') {
+    return this.queue.toString();
+  }
+  const stringfiedArray = this.queue.map(stringfier);
+  return stringfiedArray.toString();
+};
+
+Queue.prototype.dequeue = function dequeue() {
+  return this.isEmpty() ? null : this.queue.shift();
+};
+
+Queue.prototype.peek = function peek() {
+  return this.queue.length === 0 ? null : this.queue[0];
+};
+
+Queue.prototype.isEmpty = function isEmpty() {
+  return this.queue.length === 0;
+};
+export { Queue };
