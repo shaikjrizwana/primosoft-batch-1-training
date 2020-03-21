0c1fb1a900033b7b5bd726a0b7ceb57098b2a4ca
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-21 15:48:23 +0530

    feat: made changes in promiseAllProps

--- promise-all-props/promiseAllProps.js
+++ promise-all-props/promiseAllProps.js
@@ -1,6 +1,11 @@
 
-function promiseAllProps(...args) {
-  return args;
+function promiseAllProps(obj) {
+  const resolvedObject = Object.keys(obj).reduce((acc, key) => {
+    const resolvedValue = obj[key].then((result) => result);
+    acc[key]=resolvedValue;
+    return acc;
+  }, {});
+  return Promise.resolve(resolvedObject);
 }
 
 export {
