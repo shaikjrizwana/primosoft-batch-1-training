803273b170329900af860c176f70efc9b7de98d6
Divya436 <DivyaChinthala519@gmail.com>
2020-03-21 20:52:32 +0530

    feat: made changes in promiseAllProps

--- promise-all-props/promiseAllProps.js
+++ promise-all-props/promiseAllProps.js
@@ -1,6 +1,11 @@
 
-function promiseAllProps(...args) {
-  return args;
+function promiseAllProps(obj) {
+  const object = Object.keys(obj).reduce((result, key) => {
+    const value = obj[key].then((val) => val);
+    result[key] = value;
+    return result;
+  }, {});
+  return Promise.resolve(object);
 }
 
 export {
