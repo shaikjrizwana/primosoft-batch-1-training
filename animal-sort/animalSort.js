8d021935620d27e198cb751928087f6a61f32dc8
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-22 18:37:17 +0530

    feat: made changes in animalSort

--- animal-sort/animalSort.js
+++ animal-sort/animalSort.js
@@ -1,6 +1,11 @@
 
-function animalSort(...args) {
-  return args;
+function animalSort(arr) {
+  return arr.sort((a, b) => {
+    if (a.numberOfLegs === b.numberOfLegs) {
+      return a.name > b.name ? 1 : -1;
+    }
+    return a.numberOfLegs - b.numberOfLegs;
+  });
 }
 
 export {
