96235aadc0387d8e3b72b27c1e378c57790cf8c7
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-24 16:54:46 +0530

    feat: Event Handlers Description: we use all the event handlers like onMouseOver,onSubmit.

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,19 +2,17 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+   onMouseOver(){
+    alert('Mouse is moved')
+  }
   render() {
-    const age = 17;
-    const major = "Person is Major";
-    const minor = "Person is Minor";
     const fruits = ["apple", "orange", "mango"];
     return (
       <div className="App">
-        <h1>This is my first App</h1>
-        <h1> {age > 18 ? major : minor} </h1>
         <h1>Displaying list of fruits</h1>
         <h2>
           {fruits.map(fruit => {
-            return <div>{fruit}</div>;
+            return <div key={fruit} onMouseOver={this.onMouseOver}> {fruit}</div>
           })}
         </h2>
       </div>
