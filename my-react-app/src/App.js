0bfc31cd95bd1b3fa942c3873a29a4c7e4c69757
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-24 15:59:39 +0530

    feat: rendering list Description: we can display list of elements using render.

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,18 +1,23 @@
-import React ,{Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-class App extends Component{
-  render(){
-    const age=17
-    const major="Person is Major"
-    const minor="Person is Minor"
-    return(
-        <div className="App" >
-          <h1>This is my first  App</h1>
-          <h1> { age > 18 ? major: minor} </h1>
-        </div>
-          
-
+class App extends Component {
+  render() {
+    const age = 17;
+    const major = "Person is Major";
+    const minor = "Person is Minor";
+    const fruits = ["apple", "orange", "mango"];
+    return (
+      <div className="App">
+        <h1>This is my first App</h1>
+        <h1> {age > 18 ? major : minor} </h1>
+        <h1>Displaying list of fruits</h1>
+        <h2>
+          {fruits.map(fruit => {
+            return <div>{fruit}</div>;
+          })}
+        </h2>
+      </div>
     );
   }
 }
