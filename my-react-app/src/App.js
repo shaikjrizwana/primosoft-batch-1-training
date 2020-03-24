d1a42a4da7a1b0bf17855a30c54ca5b712a7f95f
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-24 15:50:53 +0530

    feat: javascript logic Description: we can  declare variable and write our own logic in render

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -3,10 +3,16 @@ import './App.css';
 
 class App extends Component{
   render(){
+    const age=17
+    const major="Person is Major"
+    const minor="Person is Minor"
     return(
         <div className="App" >
           <h1>This is my first  App</h1>
+          <h1> { age > 18 ? major: minor} </h1>
         </div>
+          
+
     );
   }
 }
