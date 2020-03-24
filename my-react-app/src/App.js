f049d70231a59bbf2cddd0c14407d9fba4e62e8a
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-24 17:02:51 +0530

    feat: Event handlers part-2

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,8 +2,11 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-   onMouseOver(){
-    alert('Mouse is moved')
+  onChange(event) {
+    console.log(event.target.value);
+  }
+  onSubmit(event) {
+    alert("submitted");
   }
   render() {
     const fruits = ["apple", "orange", "mango"];
@@ -12,9 +15,17 @@ class App extends Component {
         <h1>Displaying list of fruits</h1>
         <h2>
           {fruits.map(fruit => {
-            return <div key={fruit} onMouseOver={this.onMouseOver}> {fruit}</div>
+            return (
+              <div key={fruit} onMouseOver={this.onMouseOver}>
+                {" "}
+                {fruit}
+              </div>
+            );
           })}
         </h2>
+        <form onSubmit={this.onSubmit}>
+          <input onChange={this.onChange}></input>
+        </form>
       </div>
     );
   }
