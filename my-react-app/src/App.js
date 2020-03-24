8a432d630d3f8e09aa1b876a99d020298dc51113
Divya436 <DivyaChinthala519@gmail.com>
2020-03-24 15:53:02 +0530

    feat: Event Handlers Description: we can add different types of event handlers like onClick(), onMouseOver() etc inside our render function.

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -3,16 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function onClick(){
+    alert('clicked');
+  }
+  function onMouseOver(){
+    alert('Mouse Entered');
+  }
+  function onChange(event){
+    console.log(event.target.value)
+  }
   const list = ['x','y','z'];
   return (
     <div className="App">
       <h1>
         {
           list.map(item => {
-            return <div>{item}</div>
+            return <div key = {item} onMouseOver = {onMouseOver}>{item}</div>
           })
         }
       </h1>
+      <input onChange = {onChange}/>
     </div>
   );
 }
