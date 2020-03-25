e5efb790e40009ea58ba84b2e826f04098363075
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-25 13:00:11 +0530

    feat: creating and importing components Description: we can create our own components.

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import MyComponent from "./MyComponent"
 
 class App extends Component{
   constructor(props){
@@ -20,6 +21,9 @@ class App extends Component{
       <div className="App">
         <h1>{this.state.title}</h1>
         <button onClick={this.onClick}> Click Me!</button>
+        <div>
+          <MyComponent/>
+        </div>
       </div>
     )
   }
---
+++ my-react-app/src/MyComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class MyComponent extends Component{
+  render(){
+    return(
+      <div className="Component">
+          <h1>This is  a newly created component.</h1>
+          <h1> and it is imported into another component.</h1>
+      </div>
+    )
+  }
+}
+
+export default MyComponent;
