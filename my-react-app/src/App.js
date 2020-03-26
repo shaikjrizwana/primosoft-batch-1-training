c63950de7847c929f0071e6d0b4cb8b4bfeb416a
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-26 17:18:40 +0530

    feat: uncontrolled inputs Description: uncontrolled inputs are like general html inputs. They can have default values. we can use ref to get the entered value.

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,30 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
-import MyComponent from "./MyComponent";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.onClick = this.onClick.bind(this);
-    this.state = {
-      title: "Initial Title",
-      name: "Initial Name"
-    };
+    this.logName = this.logName.bind(this);
   }
 
-  onClick() {
-    this.setState({
-      name: "New App name ",
-      title: "New App title"
-    });
+  logName() {
+    console.log(this.name.value)
   }
   render() {
     return (
       <div className="App">
-        <MyComponent
-          name={this.state.name}
-          title={this.state.title}
-          onClick={this.onClick}
+        <input
+          defaultValue="mahendra"
+          ref={input => this.name = input}
+          onChange={this.logName}
         />
       </div>
     );
