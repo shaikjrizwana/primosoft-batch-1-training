2e3b6225c513098eaf3f6e43a616e6cec8ea4bf7
Divya436 <DivyaChinthala519@gmail.com>
2020-03-27 13:30:39 +0530

    feat: simple crud app: get Description: To build an app initially we are stroing the products object in the local storage by using localStorage.setItem we are changing the state of the component by getting those products using localStorage.getItem.

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,84 +1,47 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import React, {Component} from 'react';
+import './App.css';
+const products = [
+  {
+    name : "Redmi",
+    price : 6000
+  },
+  {
+    name : "Samsung",
+    price : 10000
+  }
+];
+localStorage.setItem('products',JSON.stringify(products));
 
-// This site has 3 pages, all of which are rendered
-// dynamically in the browser (not server rendered).
-//
-// Although the page does not ever refresh, notice how
-// React Router keeps the URL up to date as you navigate
-// through the site. This preserves the browser history,
-// making sure things like the back button and bookmarks
-// work properly.
-
-export default function BasicExample() {
-  return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-        </ul>
-
-        <hr />
-
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/(about|who)/">
-            <About />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-        </Switch>
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      products : []
+    };
+  }
+  componentWillMount(){
+    this.getProducts();
+  }
+  getProducts(){
+    const products = JSON.parse(localStorage.getItem('products'));
+    this.setState({products});
+  }
+  render(){
+    return(
+      <div className="App" >
+        <h1>Products Manager</h1>
+        {
+          this.state.products.map(product => {
+            return(
+              <div key = {product.name}>
+                <span>{product.name}</span> : <span>{product.price}</span>
+              </div>
+            );
+          })
+        }
       </div>
-    </Router>
-  );
+    );
+  }
 }
 
-// You can think of these components as "pages"
-// in your app.
-
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Dashboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
-    </div>
-  );
-}
+export default App;
