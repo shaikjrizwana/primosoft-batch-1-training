d0348b248d764c8eaa1306b204f2db44c5b1d7a3
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-27 12:35:29 +0530

    feat: React Router Description: Routing is the ability to move between different parts of an application,  when a user enters a URL or clicks an element  within the application.  using React Router module we can provide routing in our react application.  We can render another page or component without refreshing the current page.  React Router provides different types of routers:  1.BrowserRouter 2.HashRouter 3. MemoryRouter.

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,31 +1,64 @@
-import React, { Component } from "react";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "Mahendra",
-      isLoggedIn: true
-    }
-    this.renderName=this.renderName.bind(this)
-  }
-  renderName() {
-    if (!this.state.isLoggedIn) {
-      return <h1>Please Login!</h1>
-    }
-    return <h1>Welcome {this.state.name}</h1>
-  }
-
-  render() {
-    return (
-      <div className="App">
-        {
-          this.renderName()
-        }
+function BasicExample() {
+  return (
+    <Router>
+      <h1>React Routing Example</h1>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+        <hr />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
-export default App;
+// You can think of these components as "pages"
+// in your app.
+
+function Home() {
+  return (
+    <div>
+      <h2>This is Home Page</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>This is About Page</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>This is Dashboard</h2>
+    </div>
+  );
+}
+
+export default BasicExample;
