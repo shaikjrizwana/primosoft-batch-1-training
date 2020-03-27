8f8437461bcd84bb184c6358cb7ef8c0e593b76b
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-27 13:29:29 +0530

    feat: Simple CRUD App : GET Description: created a simple app which will perform CRUD Operations. CRUD: Create,Read,Update and Delete CREATE: created localStorage with products. GET : Means fetching the data from db or storage. getProducts() will fetch data from localStorage.

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,64 +1,51 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React, { Component } from "react";
+import "./App.css";
 
-function BasicExample() {
-  return (
-    <Router>
-      <h1>React Routing Example</h1>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-        </ul>
-        <hr />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-        </Switch>
+const products = [
+  {
+    name: 'ipad',
+    price: 200
+  },
+  {
+    name: 'iphone',
+    price: 650
+  }
+]
+localStorage.setItem('products', JSON.stringify(products));
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    }
+  }
+
+  UNSAFE_componentWillMount() {
+      this.getProducts()
+  }
+
+  getProducts() {
+    const products = JSON.parse(localStorage.getItem('products'))
+    this.setState({ products: products });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Product Manager</h1>
+        {
+          this.state.products.map(product => {
+            return (
+              <div key={product.name}>
+                <span>{product.name}</span> : <span>{product.price}</span>
+              </div>
+            )
+          })
+        }
       </div>
-    </Router>
-  );
+    );
+  }
 }
 
-// You can think of these components as "pages"
-// in your app.
-
-function Home() {
-  return (
-    <div>
-      <h2>This is Home Page</h2>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h2>This is About Page</h2>
-    </div>
-  );
-}
-
-function Dashboard() {
-  return (
-    <div>
-      <h2>This is Dashboard</h2>
-    </div>
-  );
-}
-
-export default BasicExample;
+export default App;
