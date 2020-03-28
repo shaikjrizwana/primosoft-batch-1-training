b62eeef291642409690c70a98cfc976886b6ea2f
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-28 14:25:52 +0530

    feat: using Redux Devtools Description: installed Redux Devtools extension in chrome. Added DevTools extesnion to the app by passing it as third argument to the createStore function.

--- my-redux-app/src/index.js
+++ my-redux-app/src/index.js
@@ -1,35 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { combineReducers, createStore } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { combineReducers, createStore } from "redux";
 
 function producstReducer(state = [], action) {
   return state;
 }
 
-function userReducer(state = '', action) {
+function userReducer(state = "", {type,payload}) {
+  switch(type){
+    case 'updateUser':
+      return payload;
+    default:
+      
+  }
   return state;
 }
 
 const allReducers = combineReducers({
   products: producstReducer,
   user: userReducer
-})
-
-const store = createStore(allReducers,{
-  products: [{name:'iphone'}],
-  user: 'Mahendra'
 });
 
-console.log(store.getState())
+const store = createStore(
+  allReducers,
+  {
+    products: [{ name: "iphone" }],
+    user: "Mahendra"
+  },
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+console.log(store.getState());
+
+const updateUserAction = {
+  type: "updateUser",
+  payload: {
+    user: "Vijay"
+  }
+};
+
+store.dispatch(updateUserAction);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
