76daa327c0361ee834cc3e7ee4309de950b46ceb
Divya436 <DivyaChinthala519@gmail.com>
2020-03-29 14:36:36 +0530

    feat: creating the store Description: In store the entire state of our application presents. It is created by using the createStore() method it takes the reducer function as a parameter. reducer(): It takes the current state of an application and perform an action, and return current state

--- redux/src/index.js
+++ redux/src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {createStore} from 'redux';
+
+function reducer(){
+  return 'Current state';
+}
+const store = createStore(reducer);
+
+console.log(store.getState());
 ReactDOM.render(
   <React.StrictMode>
     <App />
