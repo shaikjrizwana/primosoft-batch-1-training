42634d81a2dd98150a9995529d6a65df8c301dc6
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-03-30 14:43:06 +0530

    feat: mapActionsToProps Description: Its is passed as the second argument to connect function. it is also know as mapDispatchToProps. it can be object of function. using mapActionsToProps we can easily dispatch our actions using functions. no need to use disptach function. In below code the  onUpdateUser() will take the user from input as action. and the updateUser() will update the store's user with given user.

--- my-redux-app/src/App.js
+++ my-redux-app/src/App.js
@@ -2,7 +2,15 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { connect } from "react-redux";
+import {updateUser} from "./actions/user-actions"
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.onUpdateUser = this.onUpdateUser.bind(this)
+  }
+  onUpdateUser(event){
+    this.props.onUpdateUser(event.target.value)
+  }
   render() {
     console.log(this.props);
     return (
@@ -21,6 +29,8 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <input  type="text" onChange = {this.onUpdateUser}/>
+        {this.props.user}
       </div>
     );
   }
@@ -30,4 +40,8 @@ products: state.products,
 user: state.user
 })
 
-export default connect(mapStateToProps)(App);
+const mapActionsToProps ={
+  onUpdateUser: updateUser
+};
+
+export default connect(mapStateToProps , mapActionsToProps)(App);
