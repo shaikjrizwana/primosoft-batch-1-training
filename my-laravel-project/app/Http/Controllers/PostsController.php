e58099dc8e7fb06e901cde097073b758228487cc
MahendraKondadasu <mahendrakondadasu@gmail.com>
2020-04-04 13:22:52 +0530

    feat: Eloquent Description: Laravel's Eloquent is a Object Relational Mapper which privides a easy way to communicate with databases. using Eloquent we can avoid writing lengthy SQL queries. we can create Eloquent using this command php artisan make:model

--- my-laravel-project/app/Http/Controllers/PostsController.php
+++ my-laravel-project/app/Http/Controllers/PostsController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Post;
+
 class PostsController extends Controller
 {
     public function show($slug)
     {
-        $post = \DB::table('posts')->where('slug', $slug)->first();
+        $post = Post::where('slug',$slug)->firstOrFail();
         return view('post', [
             'post' => $post
         ]);
