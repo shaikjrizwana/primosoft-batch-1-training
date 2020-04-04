d31ace5ecfa8d389e8c1b8d81888f129c9daa1c0
Divya436 <DivyaChinthala519@gmail.com>
2020-04-04 17:11:53 +0530

    feat: migrations Description: Migrations are like version control to our database. To create a migration: php artisan make:migration name_of_migration Migration class contains two methods: up and down Up: it is used to add changes to our database. Down: it is used to undo our changes. To run our migrations: php artisan migrate To undo our changes: php artisan migrate:rollback

---
+++ laravel-project/database/migrations/2020_04_04_113618_create_post_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->string('slug');
+            $table->text('body');
+            $table->text('title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post');
+    }
+}
